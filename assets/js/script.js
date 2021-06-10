f7fe79334053c5097150a08830a3bd9baca90859
RD <81473475+nylesor28@users.noreply.github.com>
2021-06-10 13:39:51 -0400

    logic to define password criteria based on user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,120 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lettersLower = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toLowerCase;
-var specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var pwOptionsArr = ["upper","lower","special","numeric"]
 const passwordMin = 8;
 const passwordMax = 128;
 
-function alertUser(message){
-  alert(message);
-}
+var lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lettersLower = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toLowerCase();
+var specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var numbers ="0123456789"
 
-function confirmWithUser(message){
-  return confirm(message);
-}
+var pwCriteria = {};
+var randomSelArray = [];
+var completedSelArrary = [];
 
-var setPasswordCriteria = function() {
-    var pwCriteria ={"pwLength" : 0,
+var endProcessMsg = "The Password Generation Process has been canceled!"
+
+var resetPWCriteria = function(){
+  var pwCriteria =
+  {
+    "pwLength" : 0,
     "inclUpper" : false,
     "incLower" : false,
     "inclNumbers" : false,
     "inclSpecial" : false
   }
-  var proceed = false;
-  while(!proceed){
-  var pwLengthRsp = prompt("Enter you desired password length ("+ passwordMin + " - " + passwordMax +").");
 
-
-  //If User Clicks Cancel, notifiy user and end process
-  if (pwLengthRsp === null) {
-    alertUser("The Password Generation Process has been canceled!");
-    return null;
-  }
-
-  //parse user input to number and validate length
-  var converted = parseInt(pwLengthRsp); 
-
-  if (!converted || converted < passwordMin || converted > passwordMax) {
-    alertUser("Please enter a valid number from " + passwordMin + " - " + passwordMax );
-  }
-  else {
-    pwCriteria.pwLength = converted;
-    proceed = true;
-  }
 }
- 
 
+var alertUser = function (message){
+  alert(message);
+}
+
+var confirmWithUser = function (message){
+  return confirm(message);
+}
+
+var establishPasswordCriteria = function() {
+
+  var incLower = false;
+  var inclUpper = false;
+  var inclNumbers = false;
+  var specialCharacters = false;
+
+  var proceed = false;
+
+  while(!proceed){
+      var pwLengthRsp = prompt("Enter you desired password length ("+ passwordMin + " - " + passwordMax +").");
+      //If User Clicks Cancel, notifiy user and end process
+      if (pwLengthRsp === null) {
+        alertUser(endProcessMsg);
+        resetPWCriteria();
+        return;
+      }
+
+      //parse user input to number and validate length
+      var converted = parseInt(pwLengthRsp); 
+
+      if (!converted || converted < passwordMin || converted > passwordMax) {
+        alertUser("Please enter a valid number from " + passwordMin + " - " + passwordMax );
+      }
+      else {
+        pwCriteria.pwLength = converted;
+        proceed = true;
+      }
+  }
+  //reset proceed variable to false for next group of validation
+  proceed = false;
+
+    while(!proceed) {
+      inclLower = confirmWithUser("Would you like to include LOWERCASE Characters?");
+      inclUpper = confirmWithUser("Would you like to include UPPERCASE Characters?");
+      inclNumbers = confirmWithUser("Would you like to include NUMBERS Characters?");
+      inclSpecialCharacters = confirmWithUser("Would you like to include SPECIAL Characters?");
+    
+      console.log(inclLower, inclUpper, inclNumbers, inclSpecialCharacters)
+      if (!inclLower && !inclUpper && !inclNumbers && !inclSpecialCharacters ){
+        var tryAgain = confirmWithUser("Your password must include at least one of the the following: lowercase, uppercase, numbers or special character \n WOULD YOU LIKE TO TRY AGAIN?");
+        if(!tryAgain) {
+          resetPWCriteria();
+          alertUser(endProcessMsg);
+          return;
+        }
+   
+      }
+      else {
+        if(inclLower){
+          pwCriteria.incLower = incLower;
+          randomSelArray.push("lower");
+        }
+        if(inclUpper){
+          pwCriteria.inclUpper = inclUpper;
+          randomSelArray.push("upper");
+        }
+        if(inclNumbers){
+          pwCriteria.inclNumbers = inclNumbers;
+          randomSelArray.push("numeric");
+        }
+        if(inclSpecialCharacters){
+          pwCriteria.inclSpecialCharacters = inclSpecialCharacters;
+          randomSelArray.push("special");
+        }
+        proceed = true;
+      }
+    }
 } 
 
      
 function generatePassword(){
+  randomSelArray = [];
   var randomPW ="";
-  var isValid = false;
-
-
   
-
   
-  setPasswordCriteria()
+  resetPWCriteria();
+  establishPasswordCriteria();
 
- 
+
   return randomPW;
 }
 
